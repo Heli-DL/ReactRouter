325438296e148c60358074468c981f98e8e5ebfe
Heli-DL <heli.delorme@outlook.com>
2022-10-16 18:40:48 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         <h2>Welcome to React Router</h2>
       </header>
       <BrowserRouter>
-        <Link to="/home">Home</Link>{' '}
+        <Link to="/">Home</Link>{' '}
         <Link to="/about">About</Link>{' '}
         <Link to="/contact">Contact</Link>{' '}
         <Routes>
-          <Route exact path="/home"element={<Home />} />
+          <Route exact path="/"element={<Home />} />
           <Route path="/about"element={<About />} />
           <Route path="/contact"element={<Contact />} />
           <Route path="*" element={<NotFound />} />
